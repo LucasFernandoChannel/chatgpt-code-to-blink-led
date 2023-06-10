a186297592d8e802baafcc78893ff2e30092bf40
LucasFernandoChannel <131505597+LucasFernandoChannel@users.noreply.github.com>
2023-06-09 23:25:11 -0300

    Add main code.

---
+++ blink-led-chatgpt.ino
@@ -0,0 +1,22 @@
+// Pin connected to the LED
+const int ledPin = 3;
+
+void setup() {
+  // Set the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  
+  // Wait for 0.5 seconds
+  delay(500);
+  
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  
+  // Wait for 0.5 seconds
+  delay(500);
+}
+
